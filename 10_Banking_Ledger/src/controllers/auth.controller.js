a94dcd8a71a634755f8355ddcc8616d1ef7e0269
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-06-30 12:08:45 +0530

    Enhance user registration with email notification
    Added email service for registration email notification.

--- 10_Banking_Ledger/src/controllers/auth.controller.js
+++ 10_Banking_Ledger/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
+const emailService = require('../services/email.service');
 
 
 /**
@@ -42,8 +43,51 @@ async function  userRegisterController(req ,res){
     token
     
   })
+
+  await emailService.sendRegistrationEmail(user.email, user.name);
 }
 
+
+
+/**
+ * - User Login Controller
+ * - Post /api/auth/login
+ */
+
+async function userLoginController(req , res) {
+
+  const {email ,password} = req.body;
+
+  const user = await userModel.findOne({email}).select("+password")
+  if(!user){
+    return res.status(401).json({
+      message : "Email or password is Invalid"
+    })
+  }
+ const isValidPassword = await user.comparePassword(password);
+
+ if(!isValidPassword){
+   return res.status(401).json({
+      message : "Email or password is Invalid"
+    })
+ }
+
+ const token = jwt.sign({userId : user._id},process.env.JWT_SECRET,{expiresIn : '3d'})
+ res.cookie("token" , token)
+ res.status(200).json({
+    message:"User Login Successfully",
+    user: {
+      _id : user._id,
+      email : user.email,
+      name : user.name
+    },
+    token
+ })
+  
+}
+
+
+
 module.exports = {
-  userRegisterController
+  userRegisterController,userLoginController
 }
